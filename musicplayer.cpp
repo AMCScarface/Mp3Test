08360490517000e82f34539470158aa031902801
AMCScarface <Soldierboy_93@hotmail.it>
2014-04-01 18:28:56 +0200

    Signed-off-by: AMCScarface <Soldierboy_93@hotmail.it>

--- musicplayer.cpp
+++ musicplayer.cpp
@@ -2,6 +2,7 @@
 #include "ui_musicplayer.h"
 #include <QtGui>
 #include <QtCore>
+#include <QString>
 #include <QtMultimedia/QMediaPlayer>
 
 MusicPlayer::MusicPlayer(QWidget *parent) :
@@ -22,6 +23,15 @@ void MusicPlayer::on_pB_PlayPause_clicked()
 
     player->setVolume(50);
     player->setMedia(QUrl::fromLocalFile("C:/Users/PlakkaggioHC/Desktop/sample.mp3"));
+
+    qint64 duration = player->duration();
+
+    int seconds = (duration/1000) % 60;
+    int minutes = (duration/60000) % 60;
+    int hours = (duration/3600000) % 24;
+
+    QTime time(hours, minutes,seconds);
+    ui->lbl_totDuration->setText(time.toString());
     player->play();
 }
 
